ffcd0fcf08389aef82d750d81d1f572bb6f326b2
Kimmo Forss <kimmo.forss@microsoft.com>
2021-11-12 01:02:04 +0200

    quotes

--- deploy/scripts/prepare_region.sh
+++ deploy/scripts/prepare_region.sh
@@ -351,7 +351,7 @@ load_config_vars "${deployer_config_information}" "step"
 
 
 if [ $recover == 1 ]; then
-    if [ -n $REMOTE_STATE_SA ]; then
+    if [ -n "$REMOTE_STATE_SA" ]; then
         save_config_var "REMOTE_STATE_SA" "${deployer_config_information}"
         get_and_store_sa_details ${REMOTE_STATE_SA} "${deployer_config_information}"
         #Support running prepare_region on new host when the resources are already deployed
